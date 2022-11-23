31ae0fe123c3dfacc9ff161da3c8f69576478d2c
elbinelias <elbin.elias@gmail.com>
2022-11-23 19:00:29 +0530

    Update index.js

--- firstlambda/index.js
+++ firstlambda/index.js
@@ -1,15 +1,31 @@
+//system determination lambda
 const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient();
 const lambda = new AWS.Lambda();
 console.log('Loading function');
 
-async function getGCPdetails(search_param){
+async function getGCPdetails(search_param,search_param_id,search_param_user){
   // params to send to lambda
   const params = {
     FunctionName: 'GCPDynamoDBRecords',
     InvocationType: 'RequestResponse',
     LogType: 'None',
-    Payload: JSON.stringify({"gcpid": search_param})
+    Payload: JSON.stringify({"gcpid": search_param, "id_key": search_param_id, "id_user":search_param_user})
+  };
+  const response = await lambda.invoke(params).promise();
+  if(response.StatusCode !== 200){
+    throw new Error('Failed to get response from lambda function')
+  }
+  return JSON.parse(response.Payload);
+}
+
+async function getBIFASTdetails(search_param,search_param_id,search_param_user){
+  // params to send to lambda
+  const params = {
+    FunctionName: 'BIFASTDynamoDBRecords',
+    InvocationType: 'RequestResponse',
+    LogType: 'None',
+    Payload: JSON.stringify({"bifastid": search_param, "id_key": search_param_id, "id_user":search_param_user})
   };
   const response = await lambda.invoke(params).promise();
   if(response.StatusCode !== 200){
@@ -29,6 +45,8 @@ exports.handler = function(event, context, callback) {
         if (record.eventName == "INSERT")
         {
           var search_param = record.dynamodb.NewImage.title.S;
+          var search_param_id = record.dynamodb.Keys.id.S;
+          var search_param_user = record.dynamodb.Keys.user.S;
           var search_param_len = search_param.length;
           console.log('Length is : %j', search_param_len.toString());
           
@@ -48,8 +66,11 @@ exports.handler = function(event, context, callback) {
                       console.log('system determined is %j',element.primary);
                       if (element.primary.toString() == "gcp"){
                         check = "yes";
-                        const gcp = getGCPdetails(search_param);
+                        const gcp = getGCPdetails(search_param,search_param_id,search_param_user);
                         }
+                      else{
+                        const gcp = getBIFASTdetails(search_param,search_param_id,search_param_user);
+                      }
                       });
       		   }
       		   console.log('value before %j',check);
